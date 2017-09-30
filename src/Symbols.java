a8828ac8e1ee9db9120510d51416efc372745d89
Alex Coelho <alexcvcoelho@gmail.com>
2017-09-30 13:57:37 -0300

    List of Symbols

---
+++ src/Symbols.java
@@ -0,0 +1,46 @@
+/**
+ * Created by alexcoelho on 30/09/17.
+ */
+public class Symbols {
+    public final static int
+        EOF = 0,
+        IDENT = 1,
+        NUMBER = 2,
+        PLUS = 3,
+        MINUS = 4,
+        MULT = 5,
+        DIV = 6,
+        LT = 7,
+        LE = 8,
+        GT = 9,
+        GE = 10,
+        NEQ = 11,
+        EQ = 12,
+        ASSIGN = 13,
+        LEFTPAR = 14,
+        RIGHTPAR = 15,
+        SEMICOLON = 16,
+        VAR = 17,
+        BEGIN = 18,
+        END = 19,
+        IF = 20,
+        THEN = 21,
+        ELSE = 22,
+        ENDIF = 23,
+        COMMA = 24,
+        READ = 25,
+        WRITE = 26,
+        COLON = 27,
+        INTEGER = 18,
+        BOOLEAN = 29,
+        CHAR = 30,
+        CHARACTER = 31,
+        TRUE = 32,
+        FALSE = 33,
+        OR = 34,
+        AND = 35,
+        REMAINDER = 36,
+        NOT = 37,
+        //NEW SYMBOLS SHOULD BE ADDED HERE
+        LastSymbol = 38;
+}
